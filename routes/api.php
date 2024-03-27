266429fe4164396db0dff50ac62b037e03b5151c
Ezra Senaputra <ezrasenaputra@gmail.com>
2024-03-27 23:41:24 +0700

    [DELETE] - Delete All

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\HealthCheckController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/health-check', [HealthCheckController::class, 'index']);
-Route::get('/welcome', [HealthCheckController::class, 'test']);
-Route::post('/users', [UserController::class, 'register']);
--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\HealthCheckController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
